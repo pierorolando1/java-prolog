0828e7b55ef983b15d0c81aefd7a064b874efa72
Piero Rolando <rolandopiero63@gmail.com>
2024-08-11 13:49:39 -0500

    o

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,10 +4,24 @@
  */
 package com.example;
 
+import org.jpl7.*;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-      System.out.println("Hello World!");
+    public static void main( String[] args ) {
+      Query consultQuery = new Query(
+            "consult", 
+            new Term[] {new Atom("/app/src/main/resources/test.pl")}
+      );
+      if (!consultQuery.hasSolution()) {
+            throw new RuntimeException("Failed to consult the Prolog file.");
+      }
+
+      Query query = new Query("maricon(charlie).");
+
+      if(query.hasSolution())
+        System.out.println("Charlie is a maricon.");
+      else
+        System.out.println("No solution found.");
     }
 }
